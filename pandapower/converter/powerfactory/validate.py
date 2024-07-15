55c3d0a8e0bf09e16dd0ee477c7ddee7e43b30b6
pawellytaev <pawel.lytaev@uni-kassel.de>
2024-07-15 13:45:22 +0200

    add trafo3w diff to all diffs in pf2pp validation

--- pandapower/converter/powerfactory/validate.py
+++ pandapower/converter/powerfactory/validate.py
@@ -284,6 +284,7 @@ def validate_pf_conversion(net, is_unbalanced=False, **kwargs):
         logger.info("Maximum trafo3w loading difference between pandapower and powerfactory: %.1f "
                     "percent at trafo3w %d (%s)" % (
                         max(abs(tr3w_diff_is)), max_diff_idx, net.trafo3w.at[max_diff_idx, 'name']))
+        all_diffs["trafo3w_diff"] = trafo3w_diff
 
         
     if len(net.sgen[net.sgen.in_service]) > 0:
